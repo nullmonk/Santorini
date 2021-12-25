98843fbf2e2ea141c8597160e9d8c3cc0fca9971
micahjmartin <mjm5097@rit.edu>
2021-12-25 16:38:44 -0500

    adding tile struct

--- pkg/board.go
+++ pkg/board.go
@@ -4,25 +4,41 @@ import "fmt"
 
 const BOARD_SIZE = 5
 
-type Tile uint8
+type Tile struct {
+	Height int // 0 no building, 4 capped
+	Worker interface{}
+}
+
+func (t Tile) IsOccupied() bool {
+	if t.Height > 3 {
+		return true
+	}
+	return t.Worker != nil
+}
 
 type Board struct {
 	tiles [][]Tile
 }
 
 func NewBoard() *Board {
-	// BOARD_SIZE * BOARD_SIZE
 	b := new(Board)
-	b.tiles = make([][]Tile, BOARD_SIZE) // 5x5 board
+	b.tiles = make([][]Tile, BOARD_SIZE)
 	for i := range b.tiles {
 		b.tiles[i] = make([]Tile, BOARD_SIZE)
 	}
 	return b
 }
 
-func (b Board) GetTile(x, y int) (t Tile, err error) {
-	if x > BOARD_SIZE {
-		return 0, fmt.Errorf("x cannot be > %v", BOARD_SIZE)
+func (b Board) GetTile(x, y int) (t Tile) {
+	if x >= BOARD_SIZE || x < 0 {
+		panic(fmt.Errorf("invalid x"))
 	}
-	return
+	if y >= BOARD_SIZE || y < 0 {
+		panic(fmt.Errorf("invalid x"))
+	}
+	return b.tiles[x][y]
+}
+
+func (b *Board) SetTile(x, y int, tile Tile) (err error) {
+	return nil
 }
