49be8cd5d751b3c34ca643d501186b1f7341c637
micahjmartin <mjm5097@rit.edu>
2021-12-25 17:28:22 -0500

    adding stuff

--- ai/ai.go
+++ ai/ai.go
@@ -3,5 +3,11 @@ package ai
 import santorini "santorini/pkg"
 
 type SantoriniAI struct {
-	Team *santorini.Team
+	Team  *santorini.Team  // our team
+	Board *santorini.Board // the board
+}
+
+func (a *SantoriniAI) MyTurn() {
+	// Randomly pick a worker
+
 }
